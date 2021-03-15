97a007e4f9575c357b90c24dfeb98a6b0f6f597e
Carlos Gomez <carlos.gomez@bsc.es>
2021-03-15 13:03:28 +0100

    adding the crop_image utility function

--- dl4ds/utils.py
+++ dl4ds/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from datetime import datetime
 
@@ -73,3 +74,66 @@ class Timing():
             print(f"Timing: {checktime}")
             print(self.sep)
 
+
+def crop_image(array, size, yx=None, position=False, get_copy=False):
+    """
+    Return an square cropped version of a 2D ndarray.
+    
+    Parameters
+    ----------
+    array : 2d numpy ndarray
+        Input image.
+    size : int
+        Size of the cropped image.
+    yx : tuple of int or None, optional
+        Y,X coordinate of the center of the cropped image. If None then a random
+        position will be chosen.
+    position : bool, optional
+        If set to True return also the coordinates of the bottom-left vertex.
+    
+    Returns
+    -------
+    cropped_array : numpy ndarray
+        Cropped ndarray. By default a view is returned, unless ``get_copy``
+        is True. 
+    y, x : int
+        [position=True] Y,X coordinates.
+    """
+    size_init_y = array.shape[0]
+    size_init_x = array.shape[1]
+    
+    if array.ndim != 2:
+        raise TypeError('Input array is not a 2d array.')
+    if not isinstance(size, int):
+        raise TypeError('`Size` must be integer')
+    if size >= size_init_y or size >= size_init_x: 
+        msg = "`Size` is equal to or bigger than the initial frame size"
+        raise ValueError(msg)
+
+    # wing is added to the sides of the subframe center
+    wing = (size - 1) / 2
+
+    if yx is not None and isinstance(yx, tuple):
+        y, x = yx
+    else:
+        # random location
+        y = np.random.randint(wing + 1, size_init_y - wing + 1)
+        x = np.random.randint(wing + 1, size_init_x - wing + 1)
+
+    y0 = int(y - wing)
+    y1 = int(y + wing + 1)  # +1 cause endpoint is excluded when slicing
+    x0 = int(x - wing)
+    x1 = int(x + wing + 1)
+
+    if y0 < 0 or x0 < 0 or y1 > size_init_y or x1 > size_init_x:
+        raise RuntimeError(f'Cropped image cannot be obtained with size={size}, y={y}, x={x}')
+
+    if get_copy:
+        cropped_array = array[y0: y1, x0: x1].copy()
+    else:
+        cropped_array = array[y0: y1, x0: x1]
+
+    if position:
+        return cropped_array, y, x
+    else:
+        return cropped_array
