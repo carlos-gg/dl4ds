af0a8d688081bca48dcc08af64e8871d0803c1c2
Carlos Gomez <carlos.gomez@bsc.es>
2021-03-26 18:01:47 +0100

    updating training

--- dl4ds/training_logic.py
+++ dl4ds/training_logic.py
@@ -7,9 +7,10 @@ from plot_keras_history import plot_history
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.losses import mean_absolute_error
 from matplotlib.pyplot import show
 from .utils import Timing, list_devices
-from .data_load import data_loader, get_coords
+from .data_load import data_loader, data_loader_metasr
 
 
 def training(model_function, 
@@ -41,11 +42,11 @@ def training(model_function,
     TO-DO:
     * add other losses (SSIM, SSIM+MAE)
     * Chosing GPUs: strategy = tf.distribute.MirroredStrategy(['/gpu:0', '/gpu:1'])
-    * Choosing CPUs
     
     """
     timing = Timing()
     
+    ### vervosity
     if verbosity == 'max':
         verbose = 1
     elif verbosity == 'min':
@@ -53,37 +54,53 @@ def training(model_function,
     else:
         verbose = 0
     
+    ### devices
+    if verbose in [1 ,2]:
+        print('List of devices:')
     if device == 'GPU':
-        devices = list_devices('logical', gpu=True, verbose=True)
+        devices = list_devices('logical', gpu=True, verbose=verbose)
     elif device == 'CPU':
-        devices = list_devices('logical', gpu=False, verbose=True)
+        devices = list_devices('logical', gpu=False, verbose=verbose)
     else:
         raise ValueError('device not recognized')
+    
+    ### strategy for distribution
     strategy = tf.distribute.MirroredStrategy(devices)  
-    print ('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+    if verbose in [1 ,2]:
+        print ('Number of devices: {}'.format(strategy.num_replicas_in_sync))
     batch_size_per_replica = batch_size
     global_batch_size = batch_size_per_replica * strategy.num_replicas_in_sync
-    
+    if verbose in [1 ,2]:
+        print(f'Gloabl batch size: {global_batch_size}, per replica: {batch_size_per_replica}')
+
+    ### checking model
     model_architecture = model_function.__name__
     if model_architecture not in ['edsr', 'metasr']:
-        raise ValueError('`model_function` must be EDSR or METASR')
+        raise ValueError('`model_function` must be `edsr` or `metasr`')
 
-    ds_train = data_loader(x_train, scale=scale, batch_size=global_batch_size,
-                           array_predictors=array_predictors, 
-                           topography=topography, landocean=landocean,
-                           patch_size=patch_size, model=model_architecture, 
-                           interpolation=interpolation)
-    ds_val = data_loader(x_val, scale=scale, batch_size=global_batch_size, 
-                         array_predictors=array_predictors, 
-                         topography=topography, landocean=landocean,
-                         patch_size=patch_size, model=model_architecture, 
-                         interpolation=interpolation)
-    ds_test = data_loader(x_test, scale=scale, batch_size=global_batch_size, 
-                          array_predictors=array_predictors, 
-                          topography=topography, landocean=landocean,
-                          patch_size=patch_size, model=model_architecture, 
-                          interpolation=interpolation)
+    ### data loaders
+    if model_architecture == 'edsr':
+        ds_train = data_loader(x_train, scale=scale, batch_size=global_batch_size,
+                            array_predictors=array_predictors, 
+                            topography=topography, landocean=landocean,
+                            patch_size=patch_size, model=model_architecture, 
+                            interpolation=interpolation)
+        ds_val = data_loader(x_val, scale=scale, batch_size=global_batch_size, 
+                            array_predictors=array_predictors, 
+                            topography=topography, landocean=landocean,
+                            patch_size=patch_size, model=model_architecture, 
+                            interpolation=interpolation)
+        ds_test = data_loader(x_test, scale=scale, batch_size=global_batch_size, 
+                            array_predictors=array_predictors, 
+                            topography=topography, landocean=landocean,
+                            patch_size=patch_size, model=model_architecture, 
+                            interpolation=interpolation)
+    elif model_architecture == 'metasr':
+        ds_train = data_loader_metasr(x_train, scale, global_batch_size, patch_size)
+        ds_val = data_loader_metasr(x_val, scale, global_batch_size, patch_size)
+        ds_test = data_loader_metasr(x_test, scale, global_batch_size, patch_size)
 
+    ### number of channels
     n_channels = x_train.shape[-1]
     if topography is not None:
         n_channels += 1
@@ -92,15 +109,18 @@ def training(model_function,
     if array_predictors is not None:
         n_channels += array_predictors.shape[-1]
     
+    ### callbacks: early stopping and loss plotting
     callbacks = []
     if early_stopping:
         earlystop = EarlyStopping(monitor='val_loss', mode='min', 
-                                  patience=patience, min_delta=min_delta, verbose=verbose)
+                                  patience=patience, min_delta=min_delta, 
+                                  verbose=verbose)
         callbacks.append(earlystop)
     if plot == 'llp':
         plotlosses = livelossplot.PlotLossesKerasTF()
         callbacks.append(plotlosses) 
 
+    ### instantiating and fitting the model
     with strategy.scope():
         if model_architecture == 'edsr':
             model = model_function(scale=scale, n_channels=n_channels, **architecture_params)
@@ -109,20 +129,21 @@ def training(model_function,
         if verbose == 1:
             model.summary(line_length=150)
 
-        if isinstance(learning_rate, float):
-            optimizer = Adam(learning_rate=learning_rate)
-        elif isinstance(learning_rate, tuple):
-            # Adam optimizer with a scheduler 
-            optimizer = Adam(learning_rate=PiecewiseConstantDecay(boundaries=[lr_decay_after], 
-                                                                  values=[learning_rate[0], learning_rate[1]]))
+        if isinstance(learning_rate, tuple):
+            ### Adam optimizer with a scheduler 
+            learning_rate = PiecewiseConstantDecay(boundaries=[lr_decay_after], 
+                                                   values=[learning_rate[0], learning_rate[1]])
+        optimizer = Adam(learning_rate=learning_rate)
 
-        # Compile and train model with L1 pixel loss
-        model.compile(optimizer=optimizer, loss='mean_absolute_error')
-        fithist = model.fit(ds_train, epochs=epochs, 
+        ### compiling and training the model with L1 pixel loss
+        model.compile(optimizer=optimizer, loss=mean_absolute_error)
+        fithist = model.fit(ds_train, 
+                            epochs=epochs, 
                             steps_per_epoch=steps_per_epoch,
                             validation_data=ds_val, 
                             validation_steps=validation_steps, 
-                            verbose=verbose, callbacks=callbacks)
+                            verbose=verbose, 
+                            callbacks=callbacks)
         score = model.evaluate(ds_test, steps=test_steps, verbose=verbose)
         print(f'\nScore on the test set: {score}')
         
