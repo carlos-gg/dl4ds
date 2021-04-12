132ba999db6ad80ceb90889cbef2fbb6901a00b3
Carlos Gomez <carlos.gomez@bsc.es>
2021-04-12 10:55:31 +0200

    adding attention module

--- dl4ds/blocks.py
+++ dl4ds/blocks.py
@@ -1,12 +1,14 @@
-from tensorflow.keras.layers import Add, Conv2D, Input, Lambda
+from tensorflow.keras.layers import Add, Conv2D, Lambda
+from .attention import ChannelAttention2D
 
-
-def residual_block(x_in, filters, scaling=None):
+def residual_block(x_in, filters, scaling=None, attention=False):
     """Creates an EDSR residual block."""
     x = Conv2D(filters, (3, 3), padding='same', activation='relu')(x_in)
     x = Conv2D(filters, (3, 3), padding='same')(x)
     if scaling is not None:
         x = Lambda(lambda t: t * scaling)(x)
+    if attention:
+        x = ChannelAttention2D(x.shape[-1])(x)
     x = Add()([x_in, x])
     return x
 
