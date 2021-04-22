d5a63e56b6c51c95eec5005a000ddb38b6450828
Carlos Gomez <carlos.gomez@bsc.es>
2021-04-22 12:46:33 +0200

    updating dl4ds version

--- dl4ds/__init__.py
+++ dl4ds/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from .pxwise_metrics import *
 from .training_logic import *
@@ -7,3 +7,5 @@ from .utils import *
 from .dataloader import *
 from .resnet_int import *
 from .resnet_spc import *
+from .discriminator import * 
+from .cgan import *
