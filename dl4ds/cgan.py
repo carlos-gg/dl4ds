e8ad0d0e69abed1f50c105f325dda2264f893ce1
Carlos Gomez <carlos.gomez@bsc.es>
2021-04-23 16:28:59 +0200

    adding load_checkpoint function for CGAN models

--- dl4ds/cgan.py
+++ dl4ds/cgan.py
@@ -1,10 +1,4 @@
 """
-TO-DO:
-    * test r-int
-    * residual discriminator with arbitrary scaling factors. Fix cropping2d and strides (4x or 5x)
-    * save the generator predictions on a refernce image from the test set, once per epoch
-    * training with Horovod
-
 Conditional GAN, as described in "Image-to-Image Translation with Conditional Adversarial Networks"
 """
 
@@ -14,11 +8,50 @@ import datetime
 import tensorflow as tf
 import numpy as np
 
-from .resnet_int import rint
-from .resnet_spc import rspc
+from .resnet_int import resnet_int
+from .resnet_rec import resnet_rec
+from .resnet_spc import resnet_spc
 from .discriminator import residual_discriminator
 from .dataloader import create_pair_hr_lr
 from .utils import Timing
+
+
+def load_checkpoint(checkpoint_dir, load_epoch, scale, model='resnet_spc', 
+                    topography=None, landocean=None, 
+                    n_res_blocks=(20, 4), n_filters=64, attention=False):
+    """
+    """
+    n_channels = 1
+    if topography is not None:
+        n_channels += 1
+    if landocean is not None:
+        n_channels += 1
+
+    # generator
+    if model == 'resnet_spc':
+        generator = resnet_spc(scale=scale, n_channels=n_channels, n_filters=n_filters, 
+                               n_res_blocks=n_res_blocks[0], n_channels_out=1, attention=attention)
+    elif model == 'resnet_rec':
+        generator = resnet_rec(scale=scale, n_channels=n_channels, n_filters=n_filters, 
+                               n_res_blocks=n_res_blocks[0], n_channels_out=1, attention=attention)
+    elif model == 'resnet_int':
+        generator = resnet_int(n_channels=n_channels, n_filters=n_filters, 
+                               n_res_blocks=n_res_blocks[0], n_channels_out=1, attention=attention)
+        
+    # discriminator
+    discriminator = residual_discriminator(n_channels=n_channels, n_filters=n_filters, 
+                                           n_res_blocks=n_res_blocks[1], model=model, attention=attention)
+    
+    # optimizers
+    generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                     discriminator_optimizer=discriminator_optimizer,
+                                     generator=generator, discriminator=discriminator)
+    checkpoint.restore(checkpoint_prefix + '-' + str(load_epoch))
+    return generator, generator_optimizer, discriminator, discriminator_optimizer
 
 
 def generator_loss(disc_generated_output, gen_output, target, lambda_scaling_factor=100):
@@ -59,21 +92,23 @@ def train_step(lr_array, hr_array, generator, discriminator, generator_optimizer
     """
     Training:
     * For each example input generate an output.
-    * The discriminator receives the input_image and the generated image as the first input. The second input is the input_image and the target_image.
+    * The discriminator receives the input_image and the generated image as the first input. 
+      The second input is the input_image and the target_image.
     * Next, we calculate the generator and the discriminator loss.
-    * Then, we calculate the gradients of loss with respect to both the generator and the discriminator variables(inputs) and apply those to the optimizer.
+    * Then, we calculate the gradients of loss with respect to both the generator and the 
+      discriminator variables(inputs) and apply those to the optimizer.
     """
     lr_array = tf.cast(lr_array[tf.newaxis,...], tf.float32)
     hr_array =  tf.cast(hr_array[tf.newaxis,...], tf.float32)
     
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         # running the generator
-        gen_lr_array = generator(lr_array, training=True)
+        gen_array = generator(lr_array, training=True)
         # running the discriminator using both the reference and generated HR images
         disc_real_output = discriminator([lr_array, hr_array], training=True)
-        disc_generated_output = discriminator([lr_array, gen_lr_array], training=True)
+        disc_generated_output = discriminator([lr_array, gen_array], training=True)
         # computing the losses
-        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_lr_array, hr_array)
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_array, hr_array)
         disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
 
     generator_gradients = gen_tape.gradient(gen_total_loss,
@@ -95,7 +130,7 @@ def train_step(lr_array, hr_array, generator, discriminator, generator_optimizer
     return gen_total_loss, gen_gan_loss, gen_l1_loss, disc_loss 
 
 
-def training_cgan(x_train, epochs, model='rspc', scale=5, patch_size=50, interpolation='bicubic', topography=None, 
+def training_cgan(model, x_train, epochs, scale=5, patch_size=50, interpolation='bicubic', topography=None, 
                   landocean=None, checkpoints_frequency=5, n_res_blocks=(20, 4), n_filters=64, attention=False):
     """
     
@@ -122,16 +157,22 @@ def training_cgan(x_train, epochs, model='rspc', scale=5, patch_size=50, interpo
     if landocean is not None:
         n_channels += 1
 
+    if model not in ['resnet_spc', 'resnet_int', 'resnet_rec']:
+        raise ValueError('`model` not recognized. Must be one of the following: resnet_spc, resnet_int, resnet_rec')
+
     # generator
-    if model == 'rspc':
-        generator = rspc(scale=scale, n_channels=n_channels, n_filters=n_filters, 
+    if model == 'resnet_spc':
+        generator = resnet_spc(scale=scale, n_channels=n_channels, n_filters=n_filters, 
                          n_res_blocks=n_res_blocks[0], n_channels_out=1, attention=attention)
-    elif model == 'rint':
-        generator = rint(n_channels=n_channels, n_filters=n_filters, 
+    elif model == 'resnet_int':
+        generator = resnet_int(n_channels=n_channels, n_filters=n_filters, 
+                         n_res_blocks=n_res_blocks[0], n_channels_out=1, attention=attention)
+    elif model == 'resnet_rec':
+        generator = resnet_rec(scale=scale, n_channels=n_channels, n_filters=n_filters, 
                          n_res_blocks=n_res_blocks[0], n_channels_out=1, attention=attention)
         
     # discriminator
-    discriminator = residual_discriminator(n_channels=n_channels, n_filters=n_filters, 
+    discriminator = residual_discriminator(n_channels=n_channels, n_filters=n_filters, scale=scale,
                                            n_res_blocks=n_res_blocks[1], model=model, attention=attention)
     
     # optimizers
@@ -146,15 +187,18 @@ def training_cgan(x_train, epochs, model='rspc', scale=5, patch_size=50, interpo
                                          discriminator_optimizer=discriminator_optimizer,
                                          generator=generator, discriminator=discriminator)
     
+    if patch_size % scale != 0:
+        raise ValueError('`patch_size` must be divisible by `scale` (remainder must be zero)')
+
     for epoch in range(epochs):
         start = time.time()        
         print("Epoch: ", epoch)
-        n_samples = 10 #x_train.shape[0]
+        n_samples = x_train.shape[0]
         
         for i in range(n_samples):
             if (i + 1) % 100 == 0:
                 print('.', end='')
-       
+
             hr_array, lr_array = create_pair_hr_lr(x_train[i], tuple_predictors=None, scale=scale, topography=topography, 
                                                    landocean=landocean, patch_size=patch_size, model=model, 
                                                    interpolation=interpolation)
@@ -176,7 +220,9 @@ def training_cgan(x_train, epochs, model='rspc', scale=5, patch_size=50, interpo
                 checkpoint.save(file_prefix = checkpoint_prefix)
 
         print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1, time.time()-start))
-    checkpoint.save(file_prefix = checkpoint_prefix)
+    
+    if checkpoints_frequency is not None:
+        checkpoint.save(file_prefix = checkpoint_prefix)
 
     losses_array = np.array((gentotal, gengan, genl1, disc))
     np.save('./losses.npy', losses_array)
