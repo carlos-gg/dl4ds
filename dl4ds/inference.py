af665577107887d3efadfdf831ac295fb8b79334
Carlos Gomez <carlos.gomez@bsc.es>
2021-04-26 15:24:53 +0200

    finxing predict function for resnet_rint model

--- dl4ds/inference.py
+++ dl4ds/inference.py
@@ -13,7 +13,7 @@ def predict_with_gt(
     landocean=None, 
     predictors=None, 
     interpolation='bicubic', 
-    savepath=None):
+    save_path=None):
     """
 
     Parameters
@@ -39,7 +39,7 @@ def predict_with_gt(
     if landocean is not None:
         n_channels += 1
     
-    if model_architecture == 'rspc':
+    if model_architecture in ['resnet_spc', 'resnet_rec']:
         if topography is not None:
             topo_interp = resize_array(topography, (lr_x, lr_y), interpolation)
         if landocean is not None:
@@ -56,16 +56,16 @@ def predict_with_gt(
                 array_predictors = np.asarray(tuple_predictors)
                 array_predictors = np.rollaxis(np.squeeze(array_predictors), 0, 3)
                 x_test_lr[i, :, :, pos['pred']:n_predictors+1] = array_predictors
-            if topography is not None:                                                          #################### CHECK this
-                x_test_lr[:, :, :, pos['topo']] = topo_interp
-            if landocean is not None:
-                x_test_lr[:, :, :, pos['laoc']] = lando_interp
+        if topography is not None:                                                          
+            x_test_lr[:, :, :, pos['topo']] = topo_interp
+        if landocean is not None:
+            x_test_lr[:, :, :, pos['laoc']] = lando_interp
     
         print('Downsampled x_test shape: ', x_test_lr.shape)
 
         x_test_pred = model.predict(x_test_lr)
 
-    elif model_architecture == 'rint':
+    elif model_architecture == 'resnet_int':
         x_test_lr = np.zeros((x_test.shape[0], hr_y, hr_x, n_channels))
 
         for i in range(x_test.shape[0]):
@@ -81,16 +81,16 @@ def predict_with_gt(
                 array_predictors = np.rollaxis(np.squeeze(array_predictors), 0, 3)
                 array_predictors = resize_array(array_predictors, (hr_x, hr_y), interpolation)
                 x_test_lr[i, :, :, pos['pred']:n_predictors+1] = array_predictors
-            if topography is not None:                                                          #################### CHECK this
-                x_test_lr[:, :, :, pos['topo']] = topography
-            if landocean is not None:
-                x_test_lr[:, :, :, pos['laoc']] = landocean
+        if topography is not None:                                                         
+            x_test_lr[:, :, :, pos['topo']] = topography
+        if landocean is not None:
+            x_test_lr[:, :, :, pos['laoc']] = landocean
     
         print('Downsampled x_test shape: ', x_test_lr.shape)
         x_test_pred = model.predict(x_test_lr)
 
-    if savepath is not None:
-        name = os.path.join(savepath, 'x_test_pred.npy')
+    if save_path is not None:
+        name = os.path.join(save_path, 'x_test_pred.npy')
         np.save(name, x_test_pred.astype('float32'))
     return x_test_pred
 
@@ -102,7 +102,8 @@ def predict(
     landocean=None, 
     predictors=None, 
     interpolation='bicubic', 
-    savepath=None):
+    save_path=None,
+    save_fname='x_test_pred.npy'):
     """
 
     Parameters
@@ -115,7 +116,6 @@ def predict(
         Assumed in HR for rint and in LR (resizing happens anyway) for rspc.
     """ 
     model_architecture = model.name
-    _, size_y, size_x, _ = input_array.shape
     
     n_channels = 1
     pos = {'pred':1, 'topo':1,'laoc':1}
@@ -130,7 +130,8 @@ def predict(
     if landocean is not None:
         n_channels += 1
     
-    if model_architecture == 'rspc':
+    if model_architecture in ['resnet_spc', 'resnet_rec']:
+        _, size_y, size_x, _ = input_array.shape
         # resizing both topography and land-ocean to match x_test
         if topography is not None:
             topo_interp = resize_array(topography, (size_x, size_y), interpolation)
@@ -157,13 +158,13 @@ def predict(
         #print('Downsampled x_test shape: ', array.shape)
         array_pred = model.predict(array)
 
-    elif model_architecture == 'rint':
+    elif model_architecture == 'resnet_int':
         # upsampling via interpolation to match the HR topography
         hr_y, hr_x = topography.shape
         array = np.zeros((input_array.shape[0], hr_y, hr_x, n_channels))
 
         for i in range(input_array.shape[0]):
-            input_array_i = resize_array(input_array[i], (size_x, size_y), interpolation)
+            input_array_i = resize_array(input_array[i], (hr_x, hr_y), interpolation)
             array[i, :, :, 0] = input_array_i
             if predictors is not None:
                 # we create a tuple of 3D ndarrays [lat, lon, 1]
@@ -179,9 +180,9 @@ def predict(
             array[:, :, :, pos['laoc']] = landocean
     
         #print('Downsampled x_test shape: ', array.shape)
-        x_test_pred = model.predict(array)
+        array_pred = model.predict(array)
 
-    if savepath is not None:
-        name = os.path.join(savepath, 'x_test_pred.npy')
-        np.save(name, x_test_pred.astype('float32'))
+    if save_path is not None and save_fname is not None:
+        name = os.path.join(save_path, save_fname)
+        np.save(name, array_pred.astype('float32'))
     return array_pred
