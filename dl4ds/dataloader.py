6c9e043865c370b4b52330a9751530926e0d4cb9
Carlos Gomez <carlos.gomez@bsc.es>
2021-06-02 00:57:57 +0200

    adding DataGeneratorEns class and improving create_pair_hr_lrensemble function

--- dl4ds/dataloader.py
+++ dl4ds/dataloader.py
@@ -18,7 +18,8 @@ def create_pair_hr_lrensemble(
     tuple_predictors=None, 
     model='rclstm_spc',
     debug=False, 
-    interpolation='bicubic'):
+    interpolation='bicubic',
+    downsample_hr=False):
     """
     Create a pair of HR and LR square sub-patches. In this case, the LR
     corresponds to the ensembles of the seasonal forecast and therefore is 3D.
@@ -27,6 +28,7 @@ def create_pair_hr_lrensemble(
     hr_array = np.expand_dims(hr_array, -1)
     lr_array = np.squeeze(lr_array)
     lr_array = np.expand_dims(lr_array, -1)
+    static_array = None
     
     # if tuple_predictors is not None:
     #     # turned into a 3d ndarray, [lat, lon, variables]
@@ -36,16 +38,29 @@ def create_pair_hr_lrensemble(
     if model == "rclstm_spc":
         # cropping the lr array        
         lr_array, crop_y, crop_x = crop_array(lr_array, patch_size, yx=None, 
-                                              exclude_borders=True, position=True) 
+                                              exclude_borders=False, position=True) 
         # cropping the hr array
         wing = int(scale / 2)
-        crop_y_hr = int(crop_y * scale) - wing
-        crop_x_hr = int(crop_x * scale) - wing
-        # crop_y_hr = int(crop_y * scale)
-        # crop_x_hr = int(crop_x * scale)
+        crop_y_hr = int(crop_y * scale) 
+        crop_x_hr = int(crop_x * scale)
         patch_size_hr = int(patch_size * scale) 
         hr_array = crop_array(hr_array, patch_size_hr, yx=(crop_y_hr, crop_x_hr))
-    
+
+        if downsample_hr:
+            hr_array = resize_array(hr_array, (patch_size, patch_size), interpolation=interpolation)
+            hr_array = np.expand_dims(hr_array, -1)
+            if topography is not None:
+                pass
+        else:
+            if topography is not None:
+                topography = crop_array(topography, patch_size_hr, yx=(crop_y_hr, crop_x_hr))
+                topography = np.expand_dims(topography, -1)
+                static_array = topography
+            if landocean is not None:
+                landocean = crop_array(landocean, patch_size_hr, yx=(crop_y_hr, crop_x_hr))
+                landocean = np.expand_dims(landocean, -1)
+                static_array = np.concatenate([static_array, landocean], axis=-1)
+
     hr_array = np.asarray(hr_array, 'float32')
     lr_array = np.asarray(lr_array, 'float32')
     
@@ -54,13 +69,26 @@ def create_pair_hr_lrensemble(
         print(f'Crop X,Y in LR: {crop_x}, {crop_y}')
         print(f'Crop X,Y in HR: {crop_x_hr}, {crop_y_hr}')
 
-        ecv.plot_ndarray(hr_array, dpi=80, interactive=False, 
-                         subplot_titles=('HR cropped image'))
+        ecv.plot_ndarray(hr_array, dpi=100, interactive=False, 
+                         plot_title='HR cropped image')
 
-        ecv.plot_ndarray(lr_array[:10], dpi=80, interactive=False, 
-                         subplot_titles=('LR cropped image (first 10 slices)'))
+        ecv.plot_ndarray(tuple(lr_array), dpi=100, interactive=False, 
+                         max_static_subplot_rows=25, horizontal_padding=0.02,
+                         plot_title='LR cropped image', 
+                         share_colorbar=True, share_dynamic_range=True)
+        
+        if topography is not None:
+            ecv.plot_ndarray(topography, plot_title='HR Topography', dpi=100, 
+                             interactive=False)
+        
+        if landocean is not None:
+            ecv.plot_ndarray(landocean, plot_title='HR Land Ocean mask', dpi=100, 
+                             interactive=False)
 
-    return hr_array, lr_array
+    if static_array is not None:
+        return hr_array, lr_array, static_array
+    else:
+        return hr_array, lr_array
 
 
 def create_pair_hr_lr(
@@ -204,11 +232,11 @@ def create_pair_hr_lr(
         elif model == 'resnet_int':
             if topography is not None:
                 ecv.plot_ndarray(topography, interactive=False, dpi=80, 
-                                 subplot_titles=('Topography'))
+                                 subplot_titles=('HR Topography'))
         
             if landocean is not None:
                 ecv.plot_ndarray(landocean, interactive=False, dpi=80, 
-                                 subplot_titles=('Land Ocean mask'))
+                                 subplot_titles=('HR Land Ocean mask'))
 
         if tuple_predictors is not None:
             ecv.plot_ndarray(np.rollaxis(lr_array_predictors, 2, 0), dpi=80, interactive=False, 
@@ -219,9 +247,8 @@ def create_pair_hr_lr(
 
 class DataGenerator(tf.keras.utils.Sequence):
     """
-    Sequence are a safer way to do multiprocessing. This structure guarantees 
-    that the network will only train once on each sample per epoch which is not 
-    the case with generators. 
+    A sequence structure guarantees that the network will only train once on 
+    each sample per epoch which is not the case with generators. 
     Every Sequence must implement the __getitem__ and the __len__ methods. If 
     you want to modify your dataset between epochs you may implement 
     on_epoch_end. The method __getitem__ should return a complete batch.
@@ -229,7 +256,6 @@ class DataGenerator(tf.keras.utils.Sequence):
     """
     def __init__(self, 
         array, 
-        y_array=None,
         scale=4, 
         batch_size=32, 
         patch_size=40,
@@ -237,11 +263,12 @@ class DataGenerator(tf.keras.utils.Sequence):
         landocean=None, 
         predictors=None,
         model='resnet_spc', 
-        interpolation='bicubic'):
+        interpolation='bicubic'
+        ):
         """
         Parameters
         ----------
-        model : {'resnet_spc', 'resnet_int', 'resnet_rec', 'rclstm_spc'}
+        model : {'resnet_spc', 'resnet_int', 'resnet_rec'}
             Name of the model architecture.
         predictors : tuple of 4D ndarray 
             Tuple of predictor ndarrays with dims [nsamples, lat, lon, 1].
@@ -252,7 +279,6 @@ class DataGenerator(tf.keras.utils.Sequence):
         netcdf files lazily or memmap a numpy array
         """
         self.array = array
-        self.y_array = y_array
         self.batch_size = batch_size
         self.scale = scale
         self.patch_size = patch_size
@@ -267,13 +293,6 @@ class DataGenerator(tf.keras.utils.Sequence):
         if self.model in ['resnet_spc', 'resnet_rec']:
             if not self.patch_size % self.scale == 0:
                 raise ValueError('`patch_size` must be divisible by `scale`')
-
-        if self.model in ['resnet_spc', 'resnet_int', 'resnet_rec']:
-            self.datapairfunc = create_pair_hr_lr
-        elif self.model in ['rclstm_spc']:
-            self.datapairfunc = create_pair_hr_lrensemble
-            if self.y_array is None:
-                raise ValueError('`y_array` must be provided')
 
     def __len__(self):
         """
@@ -301,7 +320,8 @@ class DataGenerator(tf.keras.utils.Sequence):
             else:
                 tuple_predictors = None
 
-            params = dict( 
+            res = create_pair_hr_lr(
+                array=self.array[i],
                 scale=self.scale, 
                 patch_size=self.patch_size, 
                 topography=self.topography, 
@@ -309,13 +329,6 @@ class DataGenerator(tf.keras.utils.Sequence):
                 tuple_predictors=tuple_predictors,
                 model=self.model,
                 interpolation=self.interpolation)
-            if self.model in ['resnet_spc', 'resnet_int', 'resnet_rec']:
-                params['array'] = self.array[i]
-            elif self.model == 'rclstm_spc':
-                params['hr_array'] = self.y_array[i]
-                params['lr_array'] = self.array[i]
-            
-            res = self.datapairfunc(**params)
             hr_array, lr_array = res
             batch_lr_images.append(lr_array)
             batch_hr_images.append(hr_array)
@@ -323,7 +336,121 @@ class DataGenerator(tf.keras.utils.Sequence):
         batch_lr_images = np.asarray(batch_lr_images)
         batch_hr_images = np.asarray(batch_hr_images) 
         return [batch_lr_images], [batch_hr_images]
-    
+
+    def on_epoch_end(self):
+        """
+        """
+        np.random.shuffle(self.indices)
+
+
+class DataGeneratorEns(tf.keras.utils.Sequence):
+    """
+    A sequence structure guarantees that the network will only train once on 
+    each sample per epoch which is not the case with generators. 
+    Every Sequence must implement the __getitem__ and the __len__ methods. If 
+    you want to modify your dataset between epochs you may implement 
+    on_epoch_end. The method __getitem__ should return a complete batch.
+
+    """
+    def __init__(self, 
+        x_array, 
+        y_array,
+        scale=4, 
+        batch_size=32, 
+        patch_size=40,
+        topography=None, 
+        landocean=None, 
+        predictors=None,
+        model='resnet_spc', 
+        interpolation='bicubic',
+        downsample_hr=False):
+        """
+        Parameters
+        ----------
+        model : {'rclstm_spc'}
+            Name of the model architecture.
+        predictors : tuple of 4D ndarray 
+            Tuple of predictor ndarrays with dims [nsamples, lat, lon, 1].
+
+        TO-DO
+        -----
+        instead of the in-memory array, we could input the path and load the 
+        netcdf files lazily or memmap a numpy array
+        """
+        self.x_array = x_array
+        self.y_array = y_array
+        self.batch_size = batch_size
+        self.scale = scale
+        self.patch_size = patch_size
+        self.topography = topography
+        self.landocean = landocean
+        self.predictors = predictors
+        self.model = checkarg_model(model)
+        self.interpolation = interpolation
+        self.downsample_hr = downsample_hr
+        self.n = x_array.shape[0]
+        self.indices = np.random.permutation(self.n)
+
+        if self.model in ['rclstm_spc']:
+            if not self.patch_size % self.scale == 0:
+                raise ValueError('`patch_size` must be divisible by `scale`')
+
+    def __len__(self):
+        """
+        Defines the number of batches the DataGenerator can produce per epoch.
+        A common practice is to set this value to n_samples / batch_size so that 
+        the model sees the training samples at most once per epoch. 
+        """
+        n_batches = self.n // self.batch_size
+        return n_batches
+
+    def __getitem__(self, index):
+        """
+        Generate one batch of data as (X, y) value pairs where X represents the 
+        input and y represents the output.
+        """
+        self.batch_rand_idx = self.indices[
+            index * self.batch_size : (index + 1) * self.batch_size]
+        batch_hr_images = []
+        batch_lr_images = []
+        batch_static_images = []
+
+        for i in self.batch_rand_idx:   
+            if self.predictors is not None:
+                # we pass a tuple of 3D ndarrays [lat, lon, 1]
+                tuple_predictors = tuple([var[i] for var in self.predictors])
+            else:
+                tuple_predictors = None
+
+            res = create_pair_hr_lrensemble(
+                hr_array = self.y_array[i],
+                lr_array = self.x_array[i],
+                scale=self.scale, 
+                patch_size=self.patch_size, 
+                topography=self.topography, 
+                landocean=self.landocean, 
+                tuple_predictors=tuple_predictors,
+                model=self.model,
+                interpolation=self.interpolation,
+                downsample_hr = self.downsample_hr)
+            
+            if self.topography is not None or self.landocean is not None:
+                hr_array, lr_array, static_array = res
+                batch_lr_images.append(lr_array)
+                batch_hr_images.append(hr_array)
+                batch_static_images.append(static_array)
+            else:
+                hr_array, lr_array = res
+                batch_lr_images.append(lr_array)
+                batch_hr_images.append(hr_array)
+
+        batch_lr_images = np.asarray(batch_lr_images)
+        batch_hr_images = np.asarray(batch_hr_images) 
+        if self.topography is not None or self.landocean is not None:
+            batch_static_images = np.asarray(batch_static_images)
+            return [batch_lr_images, batch_static_images], [batch_hr_images]
+        else:
+            return [batch_lr_images], [batch_hr_images]
 
     def on_epoch_end(self):
         """
