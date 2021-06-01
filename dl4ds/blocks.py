992b8525538f428c8756c40d571df974d63b7f2c
Carlos Gomez <carlos.gomez@bsc.es>
2021-06-02 00:58:46 +0200

    improving the residual_block with the option of performing batch normalization

--- dl4ds/blocks.py
+++ dl4ds/blocks.py
@@ -1,25 +1,26 @@
-from tensorflow.keras.layers import Add, Conv2D, Lambda, ConvLSTM2D
+from tensorflow.keras.layers import Add, Conv2D, Lambda, ConvLSTM2D, ReLU, BatchNormalization
 from .attention import ChannelAttention2D
 
 
-def residual_block(x_in, filters, scaling=None, attention=False):
-    """Creates an EDSR residual block."""
-    x = Conv2D(filters, (3, 3), padding='same', activation='relu')(x_in)
-    x = Conv2D(filters, (3, 3), padding='same')(x)
+def residual_block(x_in, filters, scaling=None, attention=False, batchnorm=False):
+    """Create a residual block
+    """
+    if batchnorm:
+        # Standard residual block
+        x = Conv2D(filters, (3, 3), padding='same')(x_in)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = Conv2D(filters, (3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+    else:
+        # EDSR-style w/o BatchNorm
+        x = Conv2D(filters, (3, 3), padding='same', activation='relu')(x_in)
+        x = Conv2D(filters, (3, 3), padding='same')(x)
+
     if scaling is not None:
         x = Lambda(lambda t: t * scaling)(x)
     if attention:
         x = ChannelAttention2D(x.shape[-1])(x)
-    x = Add()([x_in, x])
-    return x
-
-
-def residual_convlstm_block(x_in, filters, scaling=None):
-    """ """
-    x = ConvLSTM2D(filters, (3, 3), padding='same', return_sequences=True, activation='relu')(x_in)
-    x = ConvLSTM2D(filters, (3, 3), padding='same', return_sequences=True)(x)
-    if scaling is not None:
-        x = Lambda(lambda t: t * scaling)(x)
     x = Add()([x_in, x])
     return x
 
