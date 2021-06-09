14defb438760cf0168dff381dd6b1249091630f6
Carlos Gomez <carlos.gomez@bsc.es>
2021-06-09 14:17:11 +0200

    updating resnet_spc module

--- dl4ds/resnet_spc.py
+++ dl4ds/resnet_spc.py
@@ -1,16 +1,16 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Add, Conv2D, Input, Lambda, ConvLSTM2D
+from tensorflow.keras.layers import Add, Conv2D, Input, Lambda
 from tensorflow.keras.models import Model
 
-from .blocks import residual_block, residual_convlstm_block
+from .blocks import residual_block
 
 
 def resnet_spc(scale, n_channels, n_filters, n_res_blocks, n_channels_out=1, 
                attention=False):
     """
     ResNet-SPC. ResNet with EDSR-style residual blocks and pixel shuffle 
-    post-upscaling.
+    post-upscaling
     """
     x_in = Input(shape=(None, None, n_channels))
     x = b = Conv2D(n_filters, (3, 3), padding='same')(x_in)
@@ -25,39 +25,21 @@ def resnet_spc(scale, n_channels, n_filters, n_res_blocks, n_channels_out=1,
     return Model(x_in, x, name="resnet_spc")
 
 
-def rclstm_spc(scale, n_channels, n_filters, n_res_blocks, n_channels_out=1):
-    """
-    Residual ConvLSTM-SPC. ResNet with ConvLSTM residual blocks and pixel shuffle 
-    post-upscaling.
-    """
-    x_in = Input(shape=(None, None, None, n_channels))
-    x = b = ConvLSTM2D(n_filters, (3, 3), return_sequences=True, padding='same')(x_in)
-    for i in range(n_res_blocks):
-        b = residual_convlstm_block(b, n_filters)
-    b = ConvLSTM2D(n_filters, (3, 3), return_sequences=True, padding='same')(b)
-    x = Add()([x, b])
-    x = ConvLSTM2D(n_filters, (3, 3), return_sequences=False, padding='same')(x)
-    
-    x = upsample(x, scale, n_filters)
-    x = Conv2D(n_channels_out, (3, 3), padding='same')(x)
-
-    return Model(x_in, x, name="rclstm_spc")
-
-
 def upsample(x, scale, n_filters):
     def upsample_conv(x, factor, **kwargs):
-        """Sub-pixel convolution."""
+        """Sub-pixel convolution
+        """
         x = Conv2D(n_filters * (factor ** 2), 3, padding='same', **kwargs)(x)
         return Lambda(pixel_shuffle(scale=factor))(x)
 
     if scale == 2:
         x = upsample_conv(x, 2, name='conv2d_scale_2')
     elif scale == 4:
-        x = upsample_conv(x, 2, name='conv2d_1_scale_2')
-        x = upsample_conv(x, 2, name='conv2d_2_scale_2')
+        x = upsample_conv(x, 2, name='conv2d_1of2_scale_2')
+        x = upsample_conv(x, 2, name='conv2d_2of2_scale_2')
     elif scale == 20:
-        x = upsample_conv(x, 5, name='conv2d_1_scale_5')
-        x = upsample_conv(x, 4, name='conv2d_2_scale_4')
+        x = upsample_conv(x, 5, name='conv2d_1of2_scale_5')
+        x = upsample_conv(x, 4, name='conv2d_2of2_scale_4')
     else:
         x = upsample_conv(x, scale, name='conv2d_scale_' + str(scale))
 
