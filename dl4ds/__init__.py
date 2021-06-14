3029eb4369e3e75ae924cdb65436a2b584cb76ea
Carlos Gomez <carlos.gomez@bsc.es>
2021-06-14 22:25:49 +0200

    adding conv3d_rspc model

--- dl4ds/__init__.py
+++ dl4ds/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from .metrics import *
 from .training_logic import *
@@ -10,3 +10,5 @@ from .resnet_spc import *
 from .resnet_rec import *
 from .discriminator import * 
 from .cgan import *
+from .clstm_rspc import *
+from .conv3d_rspc import *
