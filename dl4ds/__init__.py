910832acc83816fcb55889e828307996b5541fda
Carlos Gomez <carlos.gomez@bsc.es>
2021-07-05 09:07:25 +0200

    updating __init__

--- dl4ds/__init__.py
+++ dl4ds/__init__.py
@@ -1,11 +1,11 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .metrics import *
 from .training_logic import *
 from .inference import *
 from .utils import *
 from .dataloader import *
-from .resnet_preupsampling import *
-from .resnet_postupsampling import *
+from .net_preupsampling import *
+from .net_postupsampling import *
 from .discriminator import * 
 from .cgan import *
