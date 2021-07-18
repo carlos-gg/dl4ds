222e649eb82d3dc8b526dd3fe8be35bc61d5489f
Carlos Gomez <carlos.gomez@bsc.es>
2021-07-18 14:38:35 +0200

    fixing the discriminator so it handles all the model combinations in dl4ds

--- dl4ds/discriminator.py
+++ dl4ds/discriminator.py
@@ -2,11 +2,13 @@ import tensorflow as tf
 from tensorflow.keras.layers import (Input, Dropout, Dense, Conv2D, Add, Concatenate, 
                                      GlobalAveragePooling2D, Cropping2D)
 from .blocks import ResidualBlock
+from .utils import POSTUPSAMPLING_METHODS
 
 
 def residual_discriminator(n_channels, model, scale, n_filters, n_res_blocks, normalization=None, attention=False):
     """
     """
+    upsampling = model.split('_')[-1]
     x_in = Input(shape=(None, None, n_channels))    
     x_1 = b = Conv2D(n_filters, (3, 3), padding='same')(x_in)
     for i in range(n_res_blocks):
@@ -19,7 +21,7 @@ def residual_discriminator(n_channels, model, scale, n_filters, n_res_blocks, no
     for i in range(n_res_blocks):
         c = ResidualBlock(n_filters, normalization=normalization, attention=attention)(c)
 
-    if model in ['resnet_spc', 'resnet_rc', 'resnet_dc', 'recresnet_spc', 'recresnet_rc', 'recresnet_dc']:  
+    if upsampling in POSTUPSAMPLING_METHODS:  
         if scale == 5:      
             c = Conv2D(n_filters, (3, 3), padding='valid', strides=(2,2))(c)
             x_2 = Conv2D(n_filters, (3, 3), padding='valid', strides=(2,2))(c)
