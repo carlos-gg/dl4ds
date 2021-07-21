a757ea89f3b68be31ab9bc2cc3de104eea7b2ff5
Carlos Gomez <carlos.gomez@bsc.es>
2021-07-21 16:24:02 +0200

    updating the predict function to handle the case when the input is in HR (groundtruth) or LR

--- dl4ds/inference.py
+++ dl4ds/inference.py
@@ -6,10 +6,11 @@ from .utils import (resize_array, spatial_to_temporal_samples, SPATIAL_MODELS,
                     SPATIOTEMP_MODELS, POSTUPSAMPLING_METHODS)
 
 
-def predict_with_gt(
+def predict(
     model, 
-    data_test, 
+    data, 
     scale, 
+    data_in_hr=True,
     topography=None, 
     landocean=None, 
     predictors=None, 
@@ -20,17 +21,20 @@ def predict_with_gt(
     save_fname='y_hat.npy',
     return_lr=False,
     stochastic_output=False):
-    """Predict with a groudtruth. The HR gridded input is downsampled, then 
-    super-resolved or downscaled using the trained super-resolution model. 
+    """Inference with ``model``. The ``data`` is super-resolved or downscaled 
+    using the trained super-resolution network. 
 
     Parameters
     ----------
     model : tf.keras model
         Trained model.
-    data_test : numpy.ndarray
+    data : numpy.ndarray
         Batch of HR grids. 
     scale : int
         Scaling factor. 
+    data_in_hr : bool, optional
+        If True, the data is assumed to be a HR groundtruth to be downsampled. 
+        Otherwise, data is a LR gridded dataset to be downscaled.
     topography : None or 2D ndarray, optional
         Elevation data.
     landocean : None or 2D ndarray, optional
@@ -49,12 +53,15 @@ def predict_with_gt(
     upsampling = model_architecture.split('_')[-1]
 
     if time_window is not None:
-        data_test = spatial_to_temporal_samples(data_test, time_window)
+        data = spatial_to_temporal_samples(data, time_window)
 
     if model_architecture in SPATIAL_MODELS:
-        _, hr_y, hr_x, _ = data_test.shape
-        lr_x = int(hr_x / scale)
-        lr_y = int(hr_y / scale)
+        if data_in_hr:
+            n_samples, hr_y, hr_x, _ = data.shape
+            lr_x = int(hr_x / scale)
+            lr_y = int(hr_y / scale)
+        else:
+            n_samples, lr_y, lr_x, _ = data.shape
         
         n_channels = 1
         pos = {'pred':1, 'topo':1,'laoc':1}
@@ -75,10 +82,17 @@ def predict_with_gt(
             if landocean is not None:
                 # integer array can only be interpolated with nearest method
                 lando_interp = resize_array(landocean, (lr_x, lr_y), interpolation='nearest')
-            x_test_lr = np.zeros((data_test.shape[0], lr_y, lr_x, n_channels))
+            
+            x_test_lr = np.zeros((n_samples, lr_y, lr_x, n_channels))  # array for inference
         
-            for i in range(data_test.shape[0]):
-                x_test_lr[i, :, :, 0] = resize_array(data_test[i], (lr_x, lr_y), interpolation)
+            for i in range(data.shape[0]):
+                if data_in_hr:
+                    # the gridded variable is downsampled
+                    x_test_lr[i, :, :, 0] = resize_array(data[i], (lr_x, lr_y), interpolation)
+                else:
+                    # the gridded variable is in LR
+                    x_test_lr[i, :, :, 0] = data[i]
+
                 if predictors is not None:
                     # we create a tuple of 3D ndarrays [lat, lon, 1]
                     tuple_predictors = tuple([var[i] for var in predictors])
@@ -92,12 +106,16 @@ def predict_with_gt(
                 x_test_lr[:, :, :, pos['laoc']] = lando_interp        
             print('Downsampled x_test shape: ', x_test_lr.shape)
 
-        else:
-            x_test_lr = np.zeros((data_test.shape[0], hr_y, hr_x, n_channels))
+        elif upsampling == 'pin':
+            x_test_lr = np.zeros((n_samples, hr_y, hr_x, n_channels))
 
-            for i in range(data_test.shape[0]):
-                # downsampling and upsampling via interpolation
-                x_test_resized = resize_array(data_test[i], (lr_x, lr_y), interpolation)
+            for i in range(data.shape[0]):
+                if data_in_hr:
+                    # downsampling
+                    x_test_resized = resize_array(data[i], (lr_x, lr_y), interpolation)
+                else:
+                    x_test_resized = data[i]  # data in LR
+                # upsampling via interpolation
                 x_test_resized = resize_array(x_test_resized, (hr_x, hr_y), interpolation)
                 x_test_lr[i, :, :, 0] = x_test_resized
                 if predictors is not None:
@@ -115,41 +133,49 @@ def predict_with_gt(
             print('Downsampled x_test shape: ', x_test_lr.shape)
     
     elif model_architecture in SPATIOTEMP_MODELS:
-        n_samples, n_t, hr_y, hr_x, n_channels = data_test.shape
-        lr_x = int(hr_x / scale)
-        lr_y = int(hr_y / scale)
-        x_test_lr = np.zeros((n_samples, n_t, lr_y, lr_x, n_channels))
-        for i in range(n_samples):
-            x_test_lr[i] = resize_array(data_test[i], (lr_x, lr_y), interpolation, squeezed=False)
+        if data_in_hr:
+            n_samples, n_t, hr_y, hr_x, n_channels = data.shape
+            lr_x = int(hr_x / scale)
+            lr_y = int(hr_y / scale)
+        else:
+            n_samples, n_t, lr_y, lr_x, n_channels = data.shape
 
-        print('Downsampled x_test shape: ', x_test_lr.shape)
-        if topography is not None or landocean is not None:
-            topography = resize_array(topography, (lr_x, lr_y), interpolation, squeezed=False)
-            landocean = resize_array(landocean, (lr_x, lr_y), 'nearest', squeezed=False)
-            topography = np.expand_dims(topography, -1)
-            landocean = np.expand_dims(landocean, -1)
-            static_array = np.concatenate([topography, landocean], axis=-1)
-            static_array = np.expand_dims(static_array, 0)
-            static_array = np.repeat(static_array, n_samples, 0)
+        if upsampling in POSTUPSAMPLING_METHODS:
+            x_test_lr = np.zeros((n_samples, n_t, lr_y, lr_x, n_channels))  # array for inference
+            for i in range(n_samples):
+                if data_in_hr:
+                    x_test_lr[i] = resize_array(data[i], (lr_x, lr_y), interpolation, squeezed=False)
+                else:
+                    x_test_lr[i] = data[i]
 
-    elif upsampling == 'pin':
-        n_samples, n_t, hr_y, hr_x, n_channels = data_test.shape
-        lr_x = int(hr_x / scale)
-        lr_y = int(hr_y / scale)
-        x_test_lr = np.zeros((n_samples, n_t, hr_y, hr_x, n_channels))
-        for i in range(n_samples):
-            temp = resize_array(data_test[i], (lr_x, lr_y), interpolation, squeezed=False)
-            x_test_lr[i] = resize_array(temp, (hr_x, hr_y), interpolation, squeezed=False)
-        
-        print('Downsampled x_test shape: ', x_test_lr.shape)
-        if topography is not None or landocean is not None:
-            topography = np.expand_dims(topography, -1)
-            landocean = np.expand_dims(landocean, -1)
-            static_array = np.concatenate([topography, landocean], axis=-1)
-            static_array = np.expand_dims(static_array, 0)
-            static_array = np.repeat(static_array, n_samples, 0)
+            print('Downsampled x_test shape: ', x_test_lr.shape)
+            if topography is not None or landocean is not None:
+                topography = resize_array(topography, (lr_x, lr_y), interpolation, squeezed=False)
+                landocean = resize_array(landocean, (lr_x, lr_y), 'nearest', squeezed=False)
+                topography = np.expand_dims(topography, -1)
+                landocean = np.expand_dims(landocean, -1)
+                static_array = np.concatenate([topography, landocean], axis=-1)
+                static_array = np.expand_dims(static_array, 0)
+                static_array = np.repeat(static_array, n_samples, 0)
 
-    ### Casting as TF tensors
+        elif upsampling == 'pin':
+            x_test_lr = np.zeros((n_samples, n_t, hr_y, hr_x, n_channels))  # array for inference
+            for i in range(n_samples):
+                if data_in_hr:
+                    temp = resize_array(data[i], (lr_x, lr_y), interpolation, squeezed=False)
+                else:
+                    temp = data[i]
+                x_test_lr[i] = resize_array(temp, (hr_x, hr_y), interpolation, squeezed=False)
+            
+            print('Downsampled x_test shape: ', x_test_lr.shape)
+            if topography is not None or landocean is not None:
+                topography = np.expand_dims(topography, -1)
+                landocean = np.expand_dims(landocean, -1)
+                static_array = np.concatenate([topography, landocean], axis=-1)
+                static_array = np.expand_dims(static_array, 0)
+                static_array = np.repeat(static_array, n_samples, 0)
+
+    ### Casting as TF tensors --------------------------------------------------
     x_test_lr = tf.cast(x_test_lr, tf.float32)
     if model_architecture in SPATIAL_MODELS:
         inputs = x_test_lr
@@ -159,10 +185,11 @@ def predict_with_gt(
             inputs = [x_test_lr, static_array]
         else:
             inputs = x_test_lr
-    ### Inference
-    # Stochasticity via Dropout. It usually only applies when training (no values 
-    # are dropped during inference). With training=True, the layer will behave 
-    # in training mode and dropout will be applied at inference time
+    
+    ### Inference --------------------------------------------------------------
+    # Stochasticity via dropout. It usually only applies when training (no values 
+    # are dropped during inference). With training=True, the Dropout layer will 
+    # behave in training mode and dropout will be applied at inference time
     x_test_pred = model(inputs, training=stochastic_output)
     x_test_pred = x_test_pred.numpy()
 
@@ -180,110 +207,3 @@ def predict_with_gt(
     else:
         return x_test_pred
 
-
-def predict(
-    model, 
-    input_array, 
-    topography=None, 
-    landocean=None, 
-    predictors=None, 
-    interpolation='bicubic', 
-    save_path=None,
-    save_fname='x_test_pred.npy'):
-    """Predict without a groudtruth. Super-resolve or downscale a LR gridded 
-    variable using the trained super-resolution model. 
-
-    Parameters
-    ----------
-    model : tf.keras model
-        Trained model.
-    input_array : numpy.ndarray
-        Gridded data in LR.
-    predictors : tuple of 4D ndarrays 
-        Predictor variables, with dimensions [nsamples, lat, lon, 1].
-    topography : None or 2D ndarray, optional
-        Elevation data. Assumed in HR for rint and in LR (resizing happens 
-        anyway) for rspc.
-    landocean : None or 2D ndarray, optional
-        Binary land-ocean mask.
-    predictors : tuple of 4D ndarrays 
-        Predictor variables, with dimensions [nsamples, lat, lon, 1].
-    interpolation : str, optional
-        Interpolation used when upsampling/downsampling the training samples.
-        By default 'bicubic'. 
-    save_path : str or None, optional
-        If not None, the prediction (gridded variable at HR) is saved to disk.
-    save_fname : str, optional
-        Filename to complete the path were the prediciton is saved. 
-    """ 
-    model_architecture = model.name
-    
-    n_channels = 1
-    pos = {'pred':1, 'topo':1,'laoc':1}
-    if predictors is not None:
-        n_predictors = len(predictors)
-        n_channels += n_predictors   
-        pos['topo'] += n_predictors  
-        pos['laoc'] += n_predictors 
-    if topography is not None:
-        n_channels += 1
-        pos['laoc'] += 1
-    if landocean is not None:
-        n_channels += 1
-    
-    if model_architecture in ['resnet_spc', 'resnet_rc']:
-        _, size_y, size_x, _ = input_array.shape
-        # resizing both topography and land-ocean to match x_test
-        if topography is not None:
-            topo_interp = resize_array(topography, (size_x, size_y), interpolation)
-        if landocean is not None:
-            # integer array can only be interpolated with nearest method
-            lando_interp = resize_array(landocean, (size_x, size_y), interpolation='nearest')
-        array = np.zeros((input_array.shape[0], size_y, size_x, n_channels))
-    
-        for i in range(input_array.shape[0]):
-            # assuming input_array has shape (lat, lon, 1)
-            array[i, :, :, :] = input_array[i]
-            if predictors is not None:
-                # we create a tuple of 3D ndarrays [lat, lon, 1]
-                tuple_predictors = tuple([var[i] for var in predictors])
-                # turned into a 3d ndarray, [lat, lon, variables]
-                array_predictors = np.asarray(tuple_predictors)
-                array_predictors = np.rollaxis(np.squeeze(array_predictors), 0, 3)
-                array[i, :, :, pos['pred']:n_predictors+1] = array_predictors
-        if topography is not None:
-            array[:, :, :, pos['topo']] = topo_interp
-        if landocean is not None:
-            array[:, :, :, pos['laoc']] = lando_interp
-    
-        #print('Downsampled x_test shape: ', array.shape)
-        array_pred = model.predict(array)
-
-    elif model_architecture == 'resnet_bi':
-        # upsampling via interpolation to match the HR topography
-        hr_y, hr_x = topography.shape
-        array = np.zeros((input_array.shape[0], hr_y, hr_x, n_channels))
-
-        for i in range(input_array.shape[0]):
-            input_array_i = resize_array(input_array[i], (hr_x, hr_y), interpolation)
-            array[i, :, :, 0] = input_array_i
-            if predictors is not None:
-                # we create a tuple of 3D ndarrays [lat, lon, 1]
-                tuple_predictors = tuple([var[i] for var in predictors])
-                # turned into a 3d ndarray, [lat, lon, variables]
-                array_predictors = np.asarray(tuple_predictors)
-                array_predictors = np.rollaxis(np.squeeze(array_predictors), 0, 3)
-                array_predictors = resize_array(array_predictors, (hr_x, hr_y), interpolation)
-                array[i, :, :, pos['pred']:n_predictors+1] = array_predictors
-        if topography is not None:
-            array[:, :, :, pos['topo']] = topography
-        if landocean is not None:
-            array[:, :, :, pos['laoc']] = landocean
-    
-        #print('Downsampled x_test shape: ', array.shape)
-        array_pred = model.predict(array)
-
-    if save_path is not None and save_fname is not None:
-        name = os.path.join(save_path, save_fname)
-        np.save(name, array_pred.astype('float32'))
-    return array_pred
