52df77e1bd7ec49e93ed088756b7b0859ec592b7
Carlos Gomez <carlos.gomez@bsc.es>
2021-11-24 15:37:52 +0100

    updating docstrings in MinMaxScaler

--- dl4ds/preprocessing.py
+++ dl4ds/preprocessing.py
@@ -6,70 +6,40 @@ from sklearn.preprocessing._data import _handle_zeros_in_scale
 
 
 class MinMaxScaler(TransformerMixin, BaseEstimator):
-    """Transform features by scaling each feature to a given range.
+    """Transform data to a given range.
     This estimator scales and translates the data distribution such
     that it is in the given range on the training set, e.g. between
     zero and one. If NaN values are present there will be replaced by a given
     value, e.g. minus one. 
 
     The transformation is given by::
-        X_std = (X - X.min(axis=``axis``)) / (X.max(axis=``axis``) - X.min(axis=``axis``))
+        X_std = (X - X.min(axis)) / (X.max(axis) - X.min(axis))
         X_scaled = X_std * (max - min) + min
-    where min, max = feature_range.
+    where min, max = range.
     
     This transformation is often used as an alternative to zero mean,
     unit variance scaling.
     
     Parameters
     ----------
-    feature_range : tuple (min, max), default=(0, 1)
+    range : tuple (min, max), default=(0, 1)
         Desired range of transformed data.
     copy : bool, default=True
         Set to False to perform inplace row normalization and avoid a
         copy (if the input is already a numpy array).
     clip : bool, default=False
         Set to True to clip transformed values of held-out data to
-        provided `feature range`.
+        provided `range`.
     axis : int, tuple of int or None, optional
         Axis or axes along which the minimum and maximum will be computed (via 
-        ``np.nanmin`` and ``np.nanmax`` functions). 
+        ``np.nanmin`` and ``np.nanmax`` functions). If None then the new range
     fillnanto : float or int, optional
         Value to be used when filling in NaN values. 
     
-    Attributes
-    ----------
-    min_ : ndarray of shape (n_features,)
-        Per feature adjustment for minimum. Equivalent to
-        ``min - X.min(axis=0) * self.scale_``
-    scale_ : ndarray of shape (n_features,)
-        Per feature relative scaling of the data. Equivalent to
-        ``(max - min) / (X.max(axis=0) - X.min(axis=0))``
-    data_min_ : ndarray of shape (n_features,)
-        Per feature minimum seen in the data
-    data_max_ : ndarray of shape (n_features,)
-        Per feature maximum seen in the data
-    data_range_ : ndarray of shape (n_features,)
-        Per feature range ``(data_max_ - data_min_)`` seen in the data
-    n_features_in_ : int
-        Number of features seen during :term:`fit`.
-    n_samples_seen_ : int
-        The number of samples processed by the estimator. It will be reset on 
-        new calls to fit, but increments across ``partial_fit`` calls.
-    feature_names_in_ : ndarray of shape (`n_features_in_`,)
-        Names of features seen during :term:`fit`. Defined only when `X`
-        has feature names that are all strings.
-    
-    See Also
-    --------
-    minmax_scale : Equivalent function without the estimator API.
-    
     Notes
     -----
-    NaNs are treated as missing values: disregarded in fit, and maintained in
-    transform.
-    For a comparison of the different scalers, transformers, and normalizers,
-    see :ref:`examples/preprocessing/plot_all_scaling.py
-    <sphx_glr_auto_examples_preprocessing_plot_all_scaling.py>`.
+    NaNs are disregarded in fit when transforming to the new value range, and 
+    then replaced according to ``fillnanto`` in transform. 
     
     Examples
     --------
@@ -89,9 +59,9 @@ class MinMaxScaler(TransformerMixin, BaseEstimator):
     [[1.5 0. ]]
     """
 
-    def __init__(self, feature_range=(0, 1), *, copy=True, clip=False, 
-                 axis=None, fillnanto=-1):
-        self.feature_range = feature_range
+    def __init__(self, range=(0, 1), *, copy=True, clip=False, axis=None, 
+                 fillnanto=-1):
+        self.range = range
         self.copy = copy
         self.clip = clip
         self.fillnanto = fillnanto
@@ -113,17 +83,6 @@ class MinMaxScaler(TransformerMixin, BaseEstimator):
 
     def fit(self, X, y=None):
         """Compute the minimum and maximum to be used for later scaling.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            The data used to compute the per-feature minimum and maximum
-            used for later scaling along the features axis.
-        y : None
-            Ignored.
-        Returns
-        -------
-        self : object
-            Fitted scaler.
         """
         # Reset internal state before fitting
         self._reset()
@@ -131,33 +90,16 @@ class MinMaxScaler(TransformerMixin, BaseEstimator):
 
     def partial_fit(self, X, y=None):
         """Online computation of min and max on X for later scaling.
-        All of X is processed as a single batch. This is intended for cases
-        when :meth:`fit` is not feasible due to very large number of
-        `n_samples` or because X is read from a continuous stream.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            The data used to compute the mean and standard deviation
-            used for later scaling along the features axis.
-        y : None
-            Ignored.
-        Returns
-        -------
-        self : object
-            Fitted scaler.
         """
-        feature_range = self.feature_range
-        if feature_range[0] >= feature_range[1]:
+        range = self.range
+        if range[0] >= range[1]:
             raise ValueError(
                 "Minimum of desired feature range must be smaller than maximum. Got %s."
-                % str(feature_range)
+                % str(range)
             )
 
         if sparse.issparse(X):
-            raise TypeError(
-                "MinMaxScaler does not support sparse input. "
-                "Consider using MaxAbsScaler instead."
-            )
+            raise TypeError("MinMaxScaler does not support sparse input.")
 
         first_pass = not hasattr(self, "n_samples_seen_")
         
@@ -181,25 +123,17 @@ class MinMaxScaler(TransformerMixin, BaseEstimator):
             self.n_samples_seen_ += X.shape[0]
 
         data_range = data_max - data_min
-        self.scale_ = (feature_range[1] - feature_range[0]) / _handle_zeros_in_scale(
+        self.scale_ = (range[1] - range[0]) / _handle_zeros_in_scale(
             data_range, copy=True
         )
-        self.min_ = feature_range[0] - data_min * self.scale_
+        self.min_ = range[0] - data_min * self.scale_
         self.data_min_ = data_min
         self.data_max_ = data_max
         self.data_range_ = data_range
         return self
 
     def transform(self, X):
-        """Scale features of X according to feature_range.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Input data that will be transformed.
-        Returns
-        -------
-        Xt : ndarray of shape (n_samples, n_features)
-            Transformed data.
+        """Scale features of X according to range.
         """
         check_is_fitted(self)
 
@@ -208,7 +142,7 @@ class MinMaxScaler(TransformerMixin, BaseEstimator):
         X *= self.scale_
         X += self.min_
         if self.clip:
-            np.clip(X, self.feature_range[0], self.feature_range[1], out=X)
+            np.clip(X, self.range[0], self.range[1], out=X)
             
         ### filling nan values
         if np.any(np.isnan(X)):
@@ -216,15 +150,7 @@ class MinMaxScaler(TransformerMixin, BaseEstimator):
         return X
 
     def inverse_transform(self, X):
-        """Undo the scaling of X according to feature_range.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Input data that will be transformed. It cannot be sparse.
-        Returns
-        -------
-        Xt : ndarray of shape (n_samples, n_features)
-            Transformed data.
+        """Undo the scaling of X according to range.
         """
         check_is_fitted(self)
     
