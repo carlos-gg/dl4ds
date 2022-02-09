9bd9c5cf2210289aa6f026e12761e753033d8aab
Carlos Gomez <carlos.gomez@bsc.es>
2022-02-09 16:44:23 +0100

    fixing setup.py including models subpackage

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ with open(resource('README.md')) as readme_file:
 
 setup(
     name='dl4ds',
-    packages=['dl4ds'],
+    packages=['dl4ds',
+              'dl4ds.models'],
     version=VERSION,
     description='Deep Learning for empirical DownScaling',
     long_description=README,
