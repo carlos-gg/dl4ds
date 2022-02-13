8ab604e0c7bcb3e1c75bf76212ad67e78efe6e3b
Carlos Gomez <carlos.gomez@bsc.es>
2022-02-13 13:26:40 +0100

    adding unet backbone, updating version

--- dl4ds/__init__.py
+++ dl4ds/__init__.py
@@ -1,18 +1,20 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
-BACKBONE_BLOCKS = ('convnet',  # plain convolutional block w/o skip connections
-                   'resnet',  # residual convolutional blocks
-                   'densenet')  # dense convolutional blocks
-PREFIX_SAMPLE_TYPE = ('', 'rec')
+BACKBONE_BLOCKS = ['convnet',       # plain convolutional block w/o skip connections
+                   'resnet',        # residual convolutional blocks
+                   'densenet']      # dense convolutional blocks
+PREFIX_SAMPLE_TYPE = ['', 'rec']
 NETS = [p + b for p in PREFIX_SAMPLE_TYPE for b in BACKBONE_BLOCKS]
-UPSAMPLING_METHODS = ('spc',  # pixel shuffle or subpixel convolution in post-upscaling
-                      'rc',  # resize convolution in post-upscaling
-                      'dc',  # deconvolution or transposed convolution in post-upscaling
-                      'pin')  # pre-upsampling via (bicubic) interpolation
-POSTUPSAMPLING_METHODS = ('spc', 'rc', 'dc')
+UPSAMPLING_METHODS = ['spc',        # pixel shuffle or subpixel convolution in post-upscaling
+                      'rc',         # resize convolution in post-upscaling
+                      'dc',         # deconvolution or transposed convolution in post-upscaling
+                      'pin']        # pre-upsampling via (bicubic) interpolation
+POSTUPSAMPLING_METHODS = ['spc', 'rc', 'dc']
 SPATIAL_MODELS = [p + '_' + u for p in BACKBONE_BLOCKS for u in UPSAMPLING_METHODS]
 SPATIOTEMP_MODELS = ['rec' + p + '_' + u for p in BACKBONE_BLOCKS for u in UPSAMPLING_METHODS]
 MODELS = [n + '_' + u for n in NETS for u in UPSAMPLING_METHODS]
+MODELS.append('unet_pin')           # encoder decoder (unet) backbone in pre-upsampling
+SPATIAL_MODELS.append('unet_pin') 
 
 from .metrics import *
 from .inference import *
