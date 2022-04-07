809c8bacd95c776737dd2931afd70b5c1af6a9fe
Carlos Gomez <carlos.gomez@bsc.es>
2022-04-07 17:08:12 +0200

    updating version

--- dl4ds/__init__.py
+++ dl4ds/__init__.py
@@ -1,21 +1,17 @@
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 BACKBONE_BLOCKS = ['convnet',       # plain convolutional block w/o skip connections
                    'resnet',        # residual convolutional blocks
                    'densenet',      # dense convolutional blocks
-                   'convnext']      # convnext style residual blocks
-PREFIX_SAMPLE_TYPE = ['', 'rec']
-NETS = [p + b for p in PREFIX_SAMPLE_TYPE for b in BACKBONE_BLOCKS]
+                   'convnext',      # convnext style residual blocks
+                   'unet']          # unet (encoder-decoder) backbone
+
 UPSAMPLING_METHODS = ['spc',        # pixel shuffle or subpixel convolution in post-upscaling
                       'rc',         # resize convolution in post-upscaling
                       'dc',         # deconvolution or transposed convolution in post-upscaling
                       'pin']        # pre-upsampling via (bicubic) interpolation
 POSTUPSAMPLING_METHODS = ['spc', 'rc', 'dc']
-SPATIAL_MODELS = [p + '_' + u for p in BACKBONE_BLOCKS for u in UPSAMPLING_METHODS]
-SPATIOTEMP_MODELS = ['rec' + p + '_' + u for p in BACKBONE_BLOCKS for u in UPSAMPLING_METHODS]
-MODELS = [n + '_' + u for n in NETS for u in UPSAMPLING_METHODS]
-MODELS.append('unet_pin')           # encoder decoder (unet) backbone in pre-upsampling
-SPATIAL_MODELS.append('unet_pin') 
+
 
 from .metrics import *
 from .inference import *
