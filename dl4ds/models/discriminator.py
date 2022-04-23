0e142bdcad87a67abc7b45e38fe0523e6007d169
Carlos Gomez <carlos.gomez@bsc.es>
2022-04-23 23:01:09 +0200

    updated signature of residual_discriminator

--- dl4ds/models/discriminator.py
+++ dl4ds/models/discriminator.py
@@ -10,59 +10,43 @@ from .. import POSTUPSAMPLING_METHODS
 
 def residual_discriminator(
     n_channels, 
-    model, 
+    upsampling, 
+    is_spatiotemporal,
     scale, 
     lr_size,
-    n_filters, 
-    n_res_blocks, 
+    # ----- below are parameters that shall be tweaked by the user -----
+    n_filters=8, 
+    n_res_blocks=4, 
     normalization=None, 
     activation='relu',
     attention=False):
     """
     """
-    upsampling = model.split('_')[-1]
-    backbone_block = model.split('_')[0]
-
-    if backbone_block.startswith('rec'):
-        backbone_block = backbone_block[3:]
-        is_recurrent = True
-    else:
-        is_recurrent = False
-
     # Branch with the LR input array
-    if is_recurrent:
+    if is_spatiotemporal:
         x_in = Input(shape=(None, None, None, n_channels))    
     else:
         x_in = Input(shape=(None, None, n_channels))    
     
-    if is_recurrent:
+    if is_spatiotemporal:
         x_1 = b = RecurrentConvBlock(
-            n_filters, 
-            activation=activation, 
-            normalization='ln',
-            dropout_rate=0)(x_in)
+            n_filters, activation=activation, normalization='ln', dropout_rate=0)(x_in)
     else:
         x_1 = b = Conv2D(n_filters, (3, 3), padding='same')(x_in)
 
     for i in range(n_res_blocks):
-        b = ResidualBlock(
-            n_filters, 
-            normalization=normalization, 
-            attention=attention)(b)
+        b = ResidualBlock(n_filters, normalization=normalization, attention=attention)(b)
     b = Conv2D(n_filters, (3, 3), padding='same')(b)
     x_1 = Add()([x_1, b])
     
     # Branch with the HR reference or HR generated array
-    if is_recurrent:
+    if is_spatiotemporal:
         x_ref = Input(shape=(None, None, None, 1))
     else:
         x_ref = Input(shape=(None, None, 1))    
     x_2 = c = Conv2D(n_filters, (3, 3), padding='same')(x_ref)
     for i in range(n_res_blocks):
-        c = ResidualBlock(
-            n_filters, 
-            normalization=normalization, 
-            attention=attention)(c)
+        c = ResidualBlock(n_filters, normalization=normalization, attention=attention)(c)
 
     if upsampling in POSTUPSAMPLING_METHODS:  
         if scale == 5:      
@@ -80,13 +64,10 @@ def residual_discriminator(
 
     x = Concatenate()([x_1, x_2])
     
-    x = ResidualBlock(
-        x.shape[-1], 
-        normalization=normalization, 
-        attention=attention)(x)
+    x = ResidualBlock(x.shape[-1], normalization=normalization, attention=attention)(x)
     
     # global average pooling operation for spatial data
-    if is_recurrent:
+    if is_spatiotemporal:
         x = GlobalAveragePooling3D()(x)
     else:
         x = GlobalAveragePooling2D()(x)
