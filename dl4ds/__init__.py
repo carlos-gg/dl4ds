96f3d8fbbf5c2b9046187f0dede580643438044c
Carlos Gomez <carlos.gomez@bsc.es>
2022-04-26 13:11:01 +0200

    version update, new global variables

--- dl4ds/__init__.py
+++ dl4ds/__init__.py
@@ -1,21 +1,39 @@
-__version__ = "1.6.0"
+__version__ = "1.6.2"
 
-BACKBONE_BLOCKS = ['convnet',       # plain convolutional block w/o skip connections
-                   'resnet',        # residual convolutional blocks
-                   'densenet',      # dense convolutional blocks
-                   'convnext',      # convnext style residual blocks
-                   'unet']          # unet (encoder-decoder) backbone
+BACKBONE_BLOCKS = [
+'convnet',              # plain convolutional block w/o skip connections
+    'resnet',           # residual convolutional blocks
+    'densenet',         # dense convolutional blocks
+    'convnext',         # convnext style residual blocks
+    'unet']             # unet (encoder-decoder) backbone
 
-UPSAMPLING_METHODS = ['spc',        # pixel shuffle or subpixel convolution in post-upscaling
-                      'rc',         # resize convolution in post-upscaling
-                      'dc',         # deconvolution or transposed convolution in post-upscaling
-                      'pin']        # pre-upsampling via (bicubic) interpolation
+UPSAMPLING_METHODS = [
+    'spc',              # pixel shuffle or subpixel convolution in post-upscaling
+    'rc',               # resize convolution in post-upscaling
+    'dc',               # deconvolution or transposed convolution in post-upscaling
+    'pin']              # pre-upsampling via (bicubic) interpolation
 POSTUPSAMPLING_METHODS = ['spc', 'rc', 'dc']
 
 INTERPOLATION_METHODS = ['inter_area', 'nearest', 'bicubic', 'bilinear', 'lanczos']
 
-LOSS_FUNCTIONS = ['mae', 'mse', 'dssim', 'dssim_mae', 'dssim_mse', 
-                  'dssim_mae_mse', 'msdssim', 'msdssim_mae', 'msdssim_mae_mse']
+LOSS_FUNCTIONS = [
+    'mae',              # mean absolute error  
+    'mse',              # mean squarred error  
+    'dssim',            # structural dissimilarity
+    'dssim_mae',        # 0.8 * DSSIM + 0.2 * MAE
+    'dssim_mse',        # 0.8 * DSSIM + 0.2 * MSE
+    'dssim_mae_mse',    # 0.6 * DSSIM + 0.2 * MAE + 0.2 * MSE
+    'msdssim',          # multiscale structural dissimilarity
+    'msdssim_mae',      # 0.8 * MSDSSIM + 0.2 * MAE
+    'msdssim_mae_mse']  # 0.6 * MSDSSIM + 0.2 * MAE + 0.2 * MSE
+
+DROPOUT_VARIANTS = [
+    'vanilla',          # vanilla dropout
+    'gaussian',         # gaussian dropout
+    'spatial',          # spatial dropout
+    'mcdrop',           # monte carlo dropout
+    'mcgaussiandrop',   # monte carlo gaussian dropout
+    'mcspatialdrop']    # monte carlo spatial dropout
 
 from .metrics import *
 from .inference import *
